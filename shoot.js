834375bccee9a8bd4c810e7ce009c9880178d5f8
dyachkovdmitri@gmail.com <Shikaka1>
2020-01-24 18:04:28 +0300

    shooting

--- shoot.js
+++ shoot.js
@@ -1,17 +1,23 @@
-function writeCircle(xc, yc, r, sinus, righter, rp) {
-
+function writeCircle(xc, yc, r, sinus, righter, rp) {//todo universal sinus for left and right
+    let traectories = [];
     for (var y = yc - r; y < yc + r; y = y + 7) {
         x = Math.sqrt(r * r - (y - yc) * (y - yc)) + xc;
-        let x2 = -Math.sqrt(r * r - (y - yc) * (y - yc)) + xc;
+        let x2 = xc - Math.sqrt(r * r - (y - yc) * (y - yc));
 
-        if (Math.abs(sinus - sin(xc, yc, x, y)) < 0.3) {
+        if (Math.abs(sinus - sin(xc, yc, x, y)) < 0.2) {
             if (righter) {
-                laser(xc, yc, x, y, rp);
+                traectories.push(x,y)
+            //    laser(xc, yc, x, y, rp);
             } else {
-                laser(xc, yc, x2, y, rp);
+                traectories.push(x2,y)
+               // laser(xc, yc, x2, y, rp);
             }
         }
     }
+
+    let traectoryNumber = randomInt(0,traectories.length/2);
+    console.log(traectories);
+    laser(xc, yc, traectories[traectoryNumber*2], traectories[traectoryNumber*2+1], rp);
 }
 
 function sin(xc, yc, x1, y1) {
@@ -19,7 +25,6 @@ function sin(xc, yc, x1, y1) {
 }
 
 function writeSector(xc, yc, r, x1, y1, rp) {
-    console.log("SINUS ", sin(xc, yc, x1, y1));
     addUnit(900, "brown", xc, yc, 4);
     addUnit(900, "blue", x1, y1, rp);
     writeCircle(xc, yc, r, sin(xc, yc, x1, y1), x1 > xc, rp);
@@ -52,12 +57,28 @@ function addLaser(left, top) {
 function laser(x1, y1, x2, y2) {
     let self = true;
     let n = (y2 - y1) / (x2 - x1);
-    for (var i = 0; i < x2 - x1; i = i + 3) {
-        if (addLaser(x1 + i+2, y1+2+ n * i,)) {
-          if(self){console.log("SELF");continue}
-            console.log("SHOOTED");
-            break;
-        }else{self = false}
+    if (x2 > x1) {
+        for (let i = 0; i < Math.abs(x2 - x1); i = i + 3) {
+            if (addLaser(x1 + i + 2, y1 + 2 + n * i,)) {
+                if (self) {
+                    continue
+                }
+                break;
+            } else {
+                self = false
+            }
+        }
+    } else {
+        for (let i = 0; i < Math.abs(x1 - x2); i = i + 3) { //duplicate
+            if (addLaser(x1 - i - 2, y1 - 2 - n * i,)) {
+                if (self) {
+                    continue
+                }
+                break;
+            } else {
+                self = false
+            }
+        }
     }
 }
 

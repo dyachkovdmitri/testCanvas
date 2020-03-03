eae89ea52cdf1b9450c25b59758102cb7815a8f1
dyachkovdmitri@gmail.com <Shikaka1>
2020-03-03 09:52:43 +0300

    refactor

--- intersection.js
+++ intersection.js
@@ -1,48 +1,45 @@
-function onIntersect(it, unit) {
+function onIntersect(it, intersected) {
     if (it.id === 9999) {   // это указатель
         return true
     }
-    if (unit.id === 345) {   // это след
+    if (intersected.id === 345) {   // это след
         return false
     }
 
-    if (unit.fill === 'white') {   // это дом
+    if (intersected.fill === 'white') {   // это дом
         return false
     }
-    if (it.id > 19999 && it.id < 21001 && unit.id > 19999 && unit.id < 21001) {   // это ресурсы
+    if (it.id > 19999 && it.id < 21001 && intersected.id > 19999 && intersected.id < 21001) {   // это ресурсы
         return true;
     }
 
 
-    if (unit.unitType > 9) { //проверка нужен ли ресурс
-        if (init.id!==it.task.swag&&unit.unitType === it.task.unitType + 10 && it.task.now === "goto") {
+    if (intersected.unitType > 9) { //проверка нужен ли ресурс
+        if (intersected.id !== it.task.swag && intersected.unitType === it.task.unitType + 10 && it.task.now === "goto") {
 
-            let home = getNearestHome(unit);
-            if (getDist(unit.left, unit.top, home[0], home[1]) > unit.radius + 20) {//swag вне дома
+            let home = getNearestHome(intersected);
+            if (getDist(intersected.left, intersected.top, home[0], home[1]) > intersected.radius + 20) {//swag вне дома
                 console.log("TAKERESherthertherthert", it.id, it.task);
                 it.task.now = "takeRes";
-                it.task.swag = unit.id;
-                // it.task.left = null;
-                // it.task.top = null;
-            }}
-            return false
+                it.task.swag = intersected.id;
+            }
         }
-
-
-        if (it.task !== undefined && it.task !== null && it.task.unitType === unit.unitType ) { //начало добычи
-            //  console.log("MINE1");
-            it.task.now = "mine";
-            it.task.id = unit.id;
-            it.task.left = null;
-
-            it.task.top = null;
-            return true;
-        }
-
-        if (unit.unitType === GRASS) {// проход сквозь траву
-            return false
-        }
-
-
-        return true
+        return false
     }
+
+
+    if (it.task !== undefined && it.task !== null && it.task.unitType === intersected.unitType&&intersected.unitType!==undefined) { //начало добычи
+        console.log("MINE1", it.id, it.task.unitType, intersected.id, intersected.unitType);
+        it.task.now = "mine";
+        it.task.id = intersected.id;
+        it.task.left = null;
+        it.task.left = null;
+        it.task.top = null;
+        return true;
+    }
+
+    if (intersected.unitType === GRASS) {// проход сквозь траву
+        return false
+    }
+    return true
+}
